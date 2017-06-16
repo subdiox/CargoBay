016755eb9323d53593c51aebaf8a346238ad0bc4
subdiox <subdiox@gmail.com>
2017-06-16 13:29:20 +0900

    add Carthage Build

---
+++ Carthage/Build/iOS/CargoBay_Carthage.framework/Headers/CargoBay_Carthage.h
@@ -0,0 +1,19 @@
+//
+//  CargoBay_Carthage.h
+//  CargoBay_Carthage
+//
+//  Created by subdiox on 2017/06/16.
+//  Copyright © 2017年 Mattt Thompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CargoBay_Carthage.
+FOUNDATION_EXPORT double CargoBay_CarthageVersionNumber;
+
+//! Project version string for CargoBay_Carthage.
+FOUNDATION_EXPORT const unsigned char CargoBay_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CargoBay_Carthage/PublicHeader.h>
+
+
